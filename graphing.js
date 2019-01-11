eb7ed18394ef67e3db182a649042ea661237a202
Isaac <isaac@zfamily.org>
2019-01-10 20:18:27 -0700

    Times for filtering are now in utc (header and input boxes).

--- graphing.js
+++ graphing.js
@@ -61,9 +61,15 @@ if (!window.location.search.includes('season=')) {
 	async function request(endpoint) {
 		// HERE
 
-		// const response = await fetch(`${api}:8080/leaderboard/${endpoint}${window.location.search}`)
+		// const response = await fetch(`${api}/leaderboard/${endpoint}${window.location.search}`)
 
-		const response = await fetch(`${api}`, {method:"GET"})
+		let str = window.location.search
+		let str_split = str.split(/(\?|&|=|to)+/)
+		let times = str_split.filter((val) => !isNaN(val[0]))
+		let start = moment.utc(times[0]).format('YYYY-MM-DDTHH:mm:ss')
+		let end =  moment.utc(times[1]).format('YYYY-MM-DDTHH:mm:ss')
+
+		const response = await fetch(`${api}/${start}_${end}`, {method:"GET"})
 		if (response.status != 200 && response.status != 201)
 			return await response.text()
 		else
